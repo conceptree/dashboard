c727dfde2b52157dad977057517580c01e31ce93
Nuno Rodrigues <nunorodrigues@Nunos-iMac.lan>
2017-05-02 00:23:59 +0100

    Side Menu

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,8 +2,18 @@
 
 function initDashboard(dsOptions){
 
+    function initMenuInteractions(){
 
+        $('.ds-side-menu').mouseover(function(){
+            $('.ds-side-menu').addClass('menu-open');
+        }).mouseout(function(){
+            $('.ds-side-menu').removeClass('menu-open');
+        });
 
+    }
 
+    initMenuInteractions();
 
 }
+
+initDashboard();
